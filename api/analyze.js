25f6fd097c10201cc2d8a0c04fe542aad09704eb
spencerzhangwei-tech <spencer.zhangwei@gmail.com>
2026-05-17 15:01:56 +0800

    Test with res.send

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({ message: 'ok', time: Date.now() });
+  res.status(200).send(JSON.stringify({ message: 'ok', time: Date.now() }));
 };
